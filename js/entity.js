9da1da807644a8d61985c25b3f18422d46c8b4ef
J0hann5 <51026601+J0hann5@users.noreply.github.com>
2022-06-16 00:06:40 +0200

    coordinate storage
    Co-authored-by: konrad2002 <konrad2002@users.noreply.github.com>

--- js/entity.js
+++ js/entity.js
@@ -1,17 +1,10 @@
-class Entity { 
-    constructor(eType, setup, x = null, y = null) {
+class Entity {
+    constructor(eType, setup, x, y) {
         this.type = eType;
 
-        if (x === null || y === null) {
-            this.pos = {
-                x: getRandomInt(0, setup.width - 1),
-                y: getRandomInt(0, setup.height - 1)
-            }
-        } else {
-            this.pos = {
-                x: x,
-                y: y
-            }
+        this.pos = {
+            x: x,
+            y: y
         }
 
         this.data = main.setup.datas[eType];
@@ -27,12 +20,17 @@ class Entity {
         this.uuid = uuidv4();
     }
 
-    move(main) {
+    move() {
 
         var r = getRandomInt(0, 100);
 
         if (r > 0 && r <= this.data.properties.speed) {
             this.newDirection = getRandomInt(0, 3);
+
+            var index = main.coords[this.pos.x][this.pos.y].indexOf(this);
+            if (index !== -1) {
+                main.coords[this.pos.x][this.pos.y].splice(index, 1);
+            }
 
             if (this.newDirection == 0 && this.pos.y > 0) {
                 this.pos.y--;
@@ -47,6 +45,8 @@ class Entity {
                 this.pos.x--;
             }
 
+            main.coords[this.pos.x][this.pos.y].push(this);
+
         }
         this.food -= this.consumption;
     }
@@ -55,35 +55,17 @@ class Entity {
 
         var r2 = getRandomInt(0, 10000);
 
-        if (r2 > 0 && r2 <= ((this.data.properties.reproduction.childs / 720) * 10000)) {
-            var offsetX = 0;
-            var offsetY = 0;
-            if (this.data.properties.reproduction.type == "plant") {
-                while (offsetX == 0 && offsetY == 0) {
-                    var offsets = getRandomOffset();
-                    offsetX = offsets[0];
-                    offsetY = offsets[1];
-                }
-
-                if (this.pos.x >= main.setup.width && offsetX > 0) {
-                    offsetX = -1
-                }
-                if (this.pos.y >= main.setup.height && offsetY > 0) {
-                    offsetY = -1
-                }
-                if (this.pos.x <= 0 && offsetX < 0) {
-                    offsetX = 1
-                }
-                if (this.pos.y <= 0 && offsetY < 0) {
-                    offsetY = 1
-                }
-
-            }
+        if ((r2 > 0 && r2 <= this.data.properties.reproduction.childs * 100) &&
+            (
+                (this.food > this.data.properties.neededMass / 2) ||
+                (this.data.properties.reproduction.type == "plant")
+            )
+        ) {
             if (main.populations[this.data.id - 1].entities.length < 2) {
                 console.warn("can't reproduct because of missing partner");
             } else {
-                main.populations[this.data.id - 1].addEntity(this.pos.x + offsetX, this.pos.y + offsetY);
-                this.food -= this.data.properties.neededMass / 4;
+                main.populations[this.data.id - 1].addEntity();
+                this.food -= this.data.properties.neededMass / 3;
             }
         }
 
@@ -91,30 +73,39 @@ class Entity {
 
     eat() {
         main.populations.forEach(population => {
+            // if (!(
+            //         (this.data.name == "ms" && population.type == "cc") &&
+            //         (this.data.name == "cc" && population.type == "ms") &&
+            //         (this.data.name == population.type)
+            //     ) || this.data.name == "cc") {
 
-            population.entities.forEach(entity => {
-
-                if (entity.uuid != this.uuid) {
-
-
+            main.coords[this.pos.x][this.pos.y].forEach(entity => {
                     if (entity.pos.x == this.pos.x && entity.pos.y == this.pos.y) {
-                        if (entity.data.properties.strength < this.data.properties.strength) {
-                            console.log(entity.data.properties.strength + " meets " + this.data.properties.consumes);
-                            if (this.data.properties.consumes[0] <= entity.data.properties.strength && this.data.properties.consumes[1] >= entity.data.properties.strength) {
+
+                        if (entity.uuid != this.uuid) {
+                            if (entity.data.properties.strength < this.data.properties.strength) {
+                                console.log(entity.data.properties.strength + " meets " + this.data.properties.consumes);
+                                if (
+                                    this.data.properties.consumes[0] <= entity.data.properties.strength &&
+                                    this.data.properties.consumes[1] >= entity.data.properties.strength
+                                ) {
+                                    entity.dead = true;
+                                    console.log(this.data.fullName + " killed " + entity.data.fullName);
+                                    this.food += entity.data.properties.mass;
+                                }
+                            }
+
+                            if (
+                                entity.data.properties.reproduction.type == "plant" && this.data.properties.reproduction.type == "plant" &&
+                                entity.dead == false && this.dead == false
+                            ) {
+                                console.log("killed plant");
                                 entity.dead = true;
-                                console.log(this.data.fullName + " killed " + entity.data.fullName);
-                                this.food += entity.data.properties.mass;
                             }
                         }
-
-                        if (entity.data.properties.reproduction.type == "plant" && this.data.properties.reproduction.type == "plant" && entity.dead == false && this.dead == false) {
-                            console.log("killed plant");
-                            entity.dead = true;
-                        }
                     }
-                }
-
-            });
+                })
+                // }
         });
     }
 
